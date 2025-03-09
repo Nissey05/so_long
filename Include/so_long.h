87c47989287b5a8539447ac2e22eb78724cb3289
nhendrik@student.codam.nl <nils@DESKTOP-F9I6T6P.>
2025-03-10 00:19:59 +0100

    png

---
+++ Include/so_long.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhendrik <nhendrik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/09 19:33:16 by nhendrik          #+#    #+#             */
+/*   Updated: 2025/03/09 19:33:16 by nhendrik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONGH
+# define SO_LONGH
+
+# include <MLX42.h>
+
+typedef struct		s_entity
+{
+	mlx_image_t *image;
+}					t_entity;
+
+typedef struct		s_game
+{
+	mlx_t *mlx;
+	t_entity **entities;
+	int x;
+	int y;
+}					t_game;
+
+typedef struct		s_player
+{
+	int	x;
+	int	y;
+	mlx_image_t *image;
+
+}					t_player;
+
+#endif
